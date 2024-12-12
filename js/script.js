772b12eb6b689238de2a6cacdb8ba22560109f41
luiz662 <141882626+luiz662@users.noreply.github.com>
2024-12-12 20:26:44 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const diaSemana = Number(prompt("Digite um dia da semana: 1-7"))
+
+switch (diaSemana) {
+
+
+    case 1:
+        alert("Domingo")
+    case 2:
+        break;
+        alert("Segunda-feira")
+        break;
+    case 3:
+        alert("Terça-feira")
+        break;
+    case 4:
+        alert("Quarta-feira")
+        break;
+    case 5:
+        alert("Quinta-feira")
+        break;
+    case 6:
+        alert("Sexta-feira")
+        break;
+    case 7:
+        alert("Sábado")
+        break;
+    default:
+        alert("não existe essa função")
+}
+
